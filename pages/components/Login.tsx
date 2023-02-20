9e543fbbc25b660430302f59c5e5cfe0aa23be0e
jarenadams21 <jarenadams3@gmail.com>
2023-02-20 11:10:48 -0500

    login-page

--- pages/components/Login.tsx
+++ pages/components/Login.tsx
@@ -1,8 +1,55 @@
+import {useState} from 'react';
 
 export default function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+    const [isLoggingIn, setIsLoggingIn] = useState(true);
+
+    function submitHandler() {
+        if (!email || !password) {
+            setError('Please enter email and password');
+             return 
+            }
+    }
+
     return (
-        <div>
-            Login
+        <div className="flex bg-green-100 gap-2 sm:gap-4 flex-col text-xs sm:text-sm justify-center items-center">
+            <h1 className='font-extrabold select-none text-2xl sm:text-4xl uppercase'>
+                {isLoggingIn ? 'Login' : 'Register'}
+                </h1>
+            { error && <div className="w-full max-w-[40ch]
+            border-rose-400 border border-solid text-center text-rose-400 py-2">{error}</div> }
+            <input type="text"
+            placeholder="react-monkey@zaza.com . . ." 
+            value={email}
+            onChange={(e) => {
+                setEmail(e.target.value);
+            }}
+            className='outline-none
+            text-slate-900 p-2 w-full max-w-[30ch]
+            duration-300 border-b-2 border-solid border-white focus:border-indigo-300'/>
+            <input type="text"
+            placeholder="securePassword1234" 
+            value={password}
+            onChange={(e) => {
+                setPassword(e.target.value);
+            }}
+            className='outline-none
+            text-slate-900 p-2 w-full max-w-[30ch]
+            duration-300 border-b-2 border-solid border-white focus:border-indigo-300'/>
+            <button
+            onClick={submitHandler} 
+            className="w-full max-w-[30ch] border py-1 border-white-400 border-solid uppercase
+            relative after:absolute after:top-0 after:right-full after:bg-white
+            after:z-10 after:w-full after:h-full overflow-hidden hover:after:translate-x-full
+            after:duration-300">
+                <h2 className="relative z-20">Go</h2>
+                </button>
+                <h2 className="duration-300 hover:scale-110 cursor-pointer"
+                onClick={() => setIsLoggingIn(!isLoggingIn)}
+                >{isLoggingIn ? 'Login' : 'Register'}</h2>
         </div>
     )
 }

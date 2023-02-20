a07b6bcc7a3015adadeb8a0be0082b2908727b83
jarenadams21 <jarenadams3@gmail.com>
2023-02-20 11:27:16 -0500

    firebase-config

---
+++ firebase.tsx
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+
+    // Private information
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app)
+export const db = getFirestore(app)
